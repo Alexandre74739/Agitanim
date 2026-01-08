8021615da55bf943cfbbaea21bee274146b6eeda
Alexandre74739 <perezalexandre430@gmail.com>
2026-01-08 22:23:57 +0100

    tailles correction

---
+++ .history/src/components/sections/Hero_20260108201800.tsx
@@ -0,0 +1,20 @@
+import Buttons from "../common/Buttons";
+import { Link } from "react-router-dom";
+import contact from "../../assets/logo.png";
+
+function Hero() {
+  return (
+    <div className="container">
+      <h1>Découvrez le métier de l'animation</h1>
+      <p>
+        Le métier d’animateur va bien au-delà de l’enchaînement d’activités. Il
+        joue un rôle essentiel dans le développement, l’épanouissement et
+        l’inclusion des enfants, en s’appuyant au quotidien sur l’écoute,
+        l’adaptation et la créativité.
+      </p>
+      <Buttons />
+    </div>
+  );
+}
+
+export default Hero;
---
+++ .history/src/components/sections/Hero_20260108202250.tsx
@@ -0,0 +1,27 @@
+import Buttons from "../common/Buttons";
+import { Link } from "react-router-dom";
+import contact from "../../assets/icon-contact.png";
+import fleches from "../../assets/icon-fleche-double.png";
+import handicap from "../../assets/icon-handicap.png";
+
+function Hero() {
+  return (
+    <div className="container">
+      <h1>Découvrez le métier de l'animation</h1>
+      <p>
+        Le métier d’animateur va bien au-delà de l’enchaînement d’activités. Il
+        joue un rôle essentiel dans le développement, l’épanouissement et
+        l’inclusion des enfants, en s’appuyant au quotidien sur l’écoute,
+        l’adaptation et la créativité.
+      </p>
+      <Buttons />
+    </div>
+     <Link to="/contact"><img src={contact} className="contact" /></Link>
+    <Link to="/inclusion"><img src={handicap} className="handicap" /></Link>
+    <a href="#projets" className="icon-scroll">
+          <img src={fleches} alt="Scroll" />
+        </a>
+  );
+}
+
+export default Hero;
