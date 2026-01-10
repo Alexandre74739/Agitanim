017840c000dbc8495aa0d8fc308de57df7f15855
perez <perezalexandre430@gmail.com>
2026-01-10 12:27:03 +0100

    section Hero complet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,23 @@
 import Hero from "../components/sections/Hero";
+import { Reveal } from "../components/layout/Reveal";
 
 function Home() {
   return (
-    <div>
-      <Hero />
-      <h1>Salut</h1>
-      <h1>Salut</h1>
-      <h1>Salut</h1>
-      <h1>Salut</h1>
+    <div className="home-page">
+        <Hero />
+
+      {/* Section animé au scroll */}
+      <div style={{ padding: "100px 0" }}>
+        <Reveal>
+          <h1 style={{ textAlign: "center" }}>Salut, je suis une section animée</h1>
+        </Reveal>
+        
+        <div style={{ height: "50vh" }} /> {/* Espace vide temporaire pour forcer le scroll */}
+
+        <Reveal>
+          <h1 style={{ textAlign: "center" }}>Et moi j'apparais plus tard !</h1>
+        </Reveal>
+      </div>
     </div>
   );
 }
