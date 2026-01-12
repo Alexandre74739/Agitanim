60c21adebfe2eeb708d8924256ef13b4ff2186ed
perez <perezalexandre430@gmail.com>
2026-01-12 15:06:17 +0100

    footer complet

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import Limitation from "../../assets/limitation.png";
 import logo from "../../assets/logo.png";
@@ -6,32 +5,44 @@ import "./Footer.scss";
 
 function Footer() {
   return (
-    <div className="footer">
-      <img src={Limitation} alt="Limitation" />
+    <footer className="footer">
+      <img
+        src={Limitation}
+        className="footer-divider"
+        alt=""
+        aria-hidden="true"
+      />
 
       <div className="footer-container">
-        <Link to="/">
+        <Link to="/" className="footer-logo">
           <img src={logo} className="logo" alt="Logo" />
         </Link>
 
-        <p>Développé et maintenu par PEREZ Alexandre-Philippe</p>
-
-        <nav className="nav-desktop">
+        <nav className="footer-nav">
           <Link to="/">Accueil</Link>
           <Link to="/jeux">Projets & Jeux</Link>
           <Link to="/inclusion">Handicap & Inclusion</Link>
           <Link to="/contact" className="btn-contact">
-            Contact
-          </Link>
+              Contact
+            </Link>
         </nav>
 
-        <ul>
+        <div className="footer-info">
+          <p>
+            Développé et maintenu par <strong>PEREZ Alexandre-Philippe</strong>
+          </p>
+          <ul className="footer-legal">
             <li>© 2025 Alexandre-Philippe Perez</li>
-            <li><Link to="/">Tous droits réservés</Link></li>
-            <li><Link to="/">Mentions légales & RGPD</Link></li>
-        </ul>
+            <li>
+              <Link to="/">Tous droits réservés</Link>
+            </li>
+            <li>
+              <Link to="/">Mentions légales & RGPD</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
