2dfbca37acf1fe6fb7c010ce7735fd3dcc88915b
Alexandre74739 <perezalexandre430@gmail.com>
2026-01-16 22:25:21 +0100

    separator SCSS

---
+++ .history/src/pages/Inclusion_20260116215359.tsx
@@ -0,0 +1,21 @@
+import Hero from "../components/sections/Hero";
+import Separator from "../components/sections/Separator";
+
+const Inclusion = () => {
+  return (
+    <div>
+      <Hero title="Rendre l’animation accessible à tous" 
+      description="Accompagner un enfant en situation de handicap demande de la compréhension, de l’adaptation et des outils concrets. Cette section rassemble des ressources inclusives pour tous, afin de mieux accueillir chaque enfant, d’adapter les activités et de favoriser une participation réelle dans les temps de loisirs."
+      showButtons={true}
+      showIcons={false}
+      showForm={false}
+      />
+      <Separator title="Pourquoi parler de handicap et d’inclusion ?"
+      text="En centres de loisirs et en temps périscolaire, les équipes accueillent des enfants aux besoins très variés. Pourtant, les animateurs sont souvent peu formés, mal outillés, et se retrouvent seuls face à des situations complexes.
+      Cette page est née d’un constat simple : mieux comprendre, c’est déjà mieux accompagner. Et personne ne devrait se sentir démuni sur le terrain."
+    />
+    
+    </div>
+  );
+};
+export default Inclusion;
--- .history/src/pages/Inclusion_20260116215401.tsx
+++ .history/src/pages/Inclusion_20260116215401.tsx
@@ -14,7 +14,7 @@ const Inclusion = () => {
         text="En centres de loisirs et en temps périscolaire, les équipes accueillent des enfants aux besoins très variés. Pourtant, les animateurs sont souvent peu formés, mal outillés, et se retrouvent seuls face à des situations complexes.
       Cette page est née d’un constat simple : mieux comprendre, c’est déjà mieux accompagner. Et personne ne devrait se sentir démuni sur le terrain."
       />
-      
+
     </div>
   );
 };
