3a598a0374a2fc72dc5fd3f662c7f95dea2e6f6a
perez <perezalexandre430@gmail.com>
2026-01-21 14:20:06 +0100

    forms ListeHandicap + begin retain

--- src/components/sections/ListeHandicap.tsx
+++ src/components/sections/ListeHandicap.tsx
@@ -4,18 +4,19 @@ import CardInclusion from "../common/CardInclusion";
 import "./ListeHandicap.scss";
 
 function ListeHandicap() {
-    return (
-        <section className="liste-handicap">
-            <Reveal>
-                <h2>J'ai besoin d'en savoir plus sur...</h2>
-            </Reveal>    
-            <Reveal>
-                <CardInclusion />
-                <div className="forms">
-                    <img src={form1} className="form1" alt="form" />
-                <img src={form1} className="form2" alt="form" />
-                </div>
-            </Reveal>
-        </section>
-    );
-} export default ListeHandicap;
+  return (
+    <section className="liste-handicap">
+      <Reveal>
+        <h2>J'ai besoin d'en savoir plus sur...</h2>
+      </Reveal>
+      <Reveal>
+        <CardInclusion />
+        <div className="forms">
+          <img src={form1} className="form1" alt="form" />
+          <img src={form1} className="form2" alt="form" />
+        </div>
+      </Reveal>
+    </section>
+  );
+}
+export default ListeHandicap;
---
+++ src/components/sections/Retain.tsx
@@ -0,0 +1,13 @@
+import { Reveal } from "../layout/Reveal";
+import './Retain.scss';
+
+function Retain() {
+  return (
+    <section className="retain-section">
+      <Reveal>
+        <h2>Ce qu'il faut retenir</h2>
+      </Reveal>
+      </section>
+  );
+}
+export default Retain;
